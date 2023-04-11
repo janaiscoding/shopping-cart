6a95094c0a2acd34ed699f689cf827de7c7af43e
janaiscoding <jana.istrate@gmail.com>
2023-04-11 21:04:42 +0300

    Separate tests, components and snapshots

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="App">Code goes here</div>;
-}
+
+import React, { useState } from "react";
+
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
 
 export default App;
---
+++ src/__tests__/App.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+import { act } from "react-dom/test-utils";
+
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    act(() => userEvent.click(button));
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  });
+});
